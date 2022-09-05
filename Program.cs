400ad2f19f8a57342d96a334273ebdf953e44220
ahmettnuman42 <ahmetnumanecevitci@gmail.com>
2022-09-05 19:28:41 +0300

    -

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algoritm_Odevleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
